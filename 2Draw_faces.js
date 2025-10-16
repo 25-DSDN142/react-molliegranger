baea89f7ff4e4b21e46d5a2a4fcc022544b24e9b
molliegranger <Molliemaygranger@gmail.com>
2025-10-16 23:41:09 +1300

    Update 2Draw_faces.js

--- 2Draw_faces.js
+++ 2Draw_faces.js
@@ -1,10 +1,6 @@
-// ----=  Faces  =----
-/* load images here */
-function prepareInteraction() {
-  //bgImage = loadImage('/images/background.png');
-}
 
-function drawInteraction(faces, hands) {
+
+function drawInteraction(faces) {
 
   // for loop to capture if there is more than one face on the screen. This applies the same process to all faces. 
   for (let i = 0; i < faces.length; i++) {
@@ -14,98 +10,47 @@ function drawInteraction(faces, hands) {
       drawPoints(face)
     }
 
-    /*
-    Once this program has a face, it knows some things about it.
-    This includes how to draw a box around the face, and an oval. 
-    It also knows where the key points of the following parts are:
-     face.leftEye
-     face.leftEyebrow
-     face.lips
-     face.rightEye
-     face.rightEyebrow
-    */
-    // Here are some variables you may like to use. 
-    // Face basics
-    let faceCenterX = face.faceOval.centerX;
-    let faceCenterY = face.faceOval.centerY;
-    let faceWidth = face.faceOval.width;
-    let faceheight = face.faceOval.height;
-    // Left eye
-    let leftEyeCenterX = face.leftEye.centerX;
-    let leftEyeCenterY = face.leftEye.centerY;
-    let leftEyeWidth = face.leftEye.width;
-    let leftEyeHeight = face.leftEye.height;
-    // Left eyebrow
-    let leftEyebrowCenterX = face.leftEyebrow.centerX;
-    let leftEyebrowCenterY = face.leftEyebrow.centerY;
-    let leftEyebrowWidth = face.leftEyebrow.width;
-    let leftEyebrowHeight = face.leftEyebrow.height;
-
-    // Lips
-    let lipsCenterX = face.lips.centerX;
-    let lipsCenterY = face.lips.centerY;
-    let lipsWidth = face.lips.width;
-    let lipsHeight = face.lips.height;
-
-    // Right eye
-    let rightEyeCenterX = face.rightEye.centerX;
-    let rightEyeCenterY = face.rightEye.centerY;
-    let rightEyeWidth = face.rightEye.width;
-    let rightEyeHeight = face.rightEye.height;
-
-    // Right eyebrow
-    let rightEyebrowCenterX = face.rightEyebrow.centerX;
-    let rightEyebrowCenterY = face.rightEyebrow.centerY;
-    let rightEyebrowWidth = face.rightEyebrow.width;
-    let rightEyebrowHeight = face.rightEyebrow.height;
-
-    let noseTipX = face.keypoints[4].x;
-    let noseTipY = face.keypoints[4].y;
-    /*
-    Start drawing on the face here
-    */
     noStroke()
-    fill(225, 225, 0);
-    // fill(get(leftEyeCenterX, leftEyeCenterY))
+fill(0, 120, 255);
+  triangle( //top blue triangle
+    face.keypoints[109].x, face.keypoints[109].y,
+    face.keypoints [10].x, face.keypoints[10].y,
+    face.keypoints[123].x, face.keypoints[123].y,
+  );
+triangle( //bottom blue triangle
+    face.keypoints[22].x, face.keypoints[22].y,
+    face.keypoints [112].x, face.keypoints[112].y,
+    face.keypoints[169].x, face.keypoints[169].y,
+  );
+  fill(250, 22, 34);
+  triangle( //bottom red triangle
+    face.keypoints[112].x, face.keypoints[112].y,
+    face.keypoints [334].x, face.keypoints[334].y,
+    face.keypoints[169].x, face.keypoints[169].y,
+  );
+  quad( //top red square
+    face.keypoints[10].x, face.keypoints[10].y,
+    face.keypoints [297].x, face.keypoints[297].y,
+    face.keypoints[188].x, face.keypoints[188].y,
+    face.keypoints[123].x, face.keypoints[123].y,
+  );
+  stroke(0)
+  strokeWeight(3);
+   line(face.keypoints[10].x, face.keypoints[10].y,
+    face.keypoints[123].x, face.keypoints[123].y,
+  );
+   line(face.keypoints[121].x, face.keypoints[121].y,
+    face.keypoints[169].x, face.keypoints[169].y,
+  );
 
-    ellipse(leftEyeCenterX, leftEyeCenterY, leftEyeWidth, leftEyeHeight);
 
     drawPoints(face.leftEye);
     drawPoints(face.leftEyebrow);
     drawPoints(face.lips);
     drawPoints(face.rightEye);
     drawPoints(face.rightEyebrow);
-
-    // drawX(rightEyeCenterX,rightEyeCenterY);
-    // drawX(leftEyeCenterX,leftEyeCenterY);
-
-
-    // drawX(noseTipX,noseTipY); 
-
-    // drawX(face.keypoints[332].x,face.keypoints[332].y);
-    // drawX(face.keypoints[103].x,face.keypoints[103].y);
-
-
-    /*
-    Stop drawing on the face here
-    */
-
   }
-  //------------------------------------------------------
-  // You can make addtional elements here, but keep the face drawing inside the for loop. 
 }
-
-function drawX(X, Y) {
-  push()
-
-  strokeWeight(15)
-  line(X - 20, Y - 20, X + 20, Y + 20)
-  line(X - 20, Y + 20, X + 20, Y - 20)
-
-  pop()
-}
-
-
 // This function draw's a dot on all the keypoints. It can be passed a whole face, or part of one. 
 function drawPoints(feature) {
 
@@ -114,7 +59,7 @@ function drawPoints(feature) {
     let element = feature.keypoints[i];
     noStroke();
     fill(0, 255, 0);
-    circle(element.x, element.y, 5);
+    circle(element.x, element.y, 0);
   }
   pop()
 
